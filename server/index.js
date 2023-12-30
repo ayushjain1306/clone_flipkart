953271ab6b5dea0319bec20aaab39f6c708c9a57
ayushjain1306 <122806933+ayushjain1306@users.noreply.github.com>
2023-12-30 20:16:45 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,6 +7,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Methods", 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Content-Type, Authorization');
+    res.header("Access-Control-Allow-Credentials", 'true');
+    next();
+})
+
 app.use(cors({
     origin: "https://clone-flipkart-mern-stack.vercel.app",
     methods: ["POST", "GET", "PUT", "DELETE"],
