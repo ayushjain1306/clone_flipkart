1e258695e80ea93637dbbe95e6315d5e8d2e26a9
ayushjain1306 <ayushtarunajain@gmail.com>
2023-12-30 18:02:00 +0530

    Edit in redux

--- client/src/redux/actions/cartActions.js
+++ client/src/redux/actions/cartActions.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import * as actionTypes from "../constants/cartConstants.js";
 
-const URL = "http://localhost:8000";
+const URL = "clone-flipkart-mern.vercel.app";
 
 export const addToCart = (id, quantity) => async (dispatch) =>{
     try {
