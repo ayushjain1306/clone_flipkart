826cd04902aa2460e9e372ecc1970e072d47ccad
ayushjain1306 <122806933+ayushjain1306@users.noreply.github.com>
2024-01-13 17:17:27 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -3,7 +3,7 @@ const DefaultData = require('./defaultData.js')
 require('dotenv').config();
 
 const connection = async ()=>{
-    const URL = "mongodb+srv://ayushjain:a9518217103@products.iokj8ph.mongodb.net/?retryWrites=true&w=majority";
+    const URL = process.env.URL;
 
     try {
         await mongoose.connect(URL, {
